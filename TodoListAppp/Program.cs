beb1bb9bf71d41f507c113d04ebbf7ac36f7aeb1
Mila <shndruk1982@gmail.com>
2025-09-27 10:29:34 +0300

    Refactor: Update welcome message

--- TodoListAppp/Program.cs
+++ TodoListAppp/Program.cs
@@ -7,7 +7,7 @@ namespace TodoListAppp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the To-Do List Application!");
+            Console.WriteLine("Welcome to the Enhanced To - Do List!");
 
             // Создаем экземпляр менеджера списка дел
             var todoManager = new TodoListManager();
